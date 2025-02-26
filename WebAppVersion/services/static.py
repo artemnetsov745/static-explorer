c4be33d28764eb5275ad72aa0ab1a526531171d2
Nikita_Tarasov <n.e.tarasov@ya.ru>
2025-02-26 19:39:31 +0300

    Add exceptions module to import

--- WebAppVersion/services/static.py
+++ WebAppVersion/services/static.py
@@ -9,7 +9,7 @@ from urllib.parse import urlparse, urljoin
 import requests
 from bs4 import BeautifulSoup
 
-from exceptions.static import FailedToDownloadFileException
+from .exceptions import FailedToDownloadFileException
 
 
 @dataclasses.dataclass
