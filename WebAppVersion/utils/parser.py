c90446831d5d9962fb2c88e953478f15c5b779c7
Nikita_Tarasov <n.e.tarasov@ya.ru>
2025-02-26 19:46:02 +0300

    Corrected names in parser text

--- WebAppVersion/utils/parser.py
+++ WebAppVersion/utils/parser.py
@@ -4,13 +4,13 @@ import argparse
 def InitParser():
     parser = argparse.ArgumentParser(description='A console program for determining the versions of web applications based on their static resources')
     # Required argument - the URL of the target site
-    parser.add_argument("url", help="The URL of the target site")
+    parser.add_argument("URI", help="The URI of the target site")
 
     # Required argument - link to the git repository
-    parser.add_argument("git", help="Link to the git repository")
+    parser.add_argument("Git", help="Link to the git repository")
 
     # Required argument - the path to the folder in the git repository
-    parser.add_argument("dir", help="The path to the folder in the git repository")
+    parser.add_argument("Dir", help="The path to the folder in the git repository")
 
     # Optional argument - extensions of the target files
     parser.add_argument("-e", "--extensions", nargs='*', help="Extensions of the target files")
